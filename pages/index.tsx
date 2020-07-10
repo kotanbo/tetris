6d512e4e57b2fdbdc2e8fee44e59b2817c5242dd
kota okamoto <kota_okamoto@kotanbo-works.com>
2020-07-10 16:24:34 +0900

    [add]Don't use scaling

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,10 @@ export const Home = (): JSX.Element => (
     <Head>
       <title>Tetris</title>
       <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+      ></meta>
     </Head>
 
     <main>
